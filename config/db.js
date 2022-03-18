d02029eff486e68f40ef0a3e6de84608d21fb9db
Faraz Khalil <mughalfrazk@gmail.com>
2022-03-18 06:22:08 +0500

    live db

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: '192.169.176.117',
+  user: 'adminchr_campaign',
+  password: ')_GF0.[I7@3b',
   database: 'campaign',
 });
 
